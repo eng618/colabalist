864450fee7daae48ca43ffced581067700a0a7fb
eng618 <eng618@fullsail.edu>
2014-04-09 02:16:20 -0400

    Added web view and WebViewController, to display modal web view from about view

---
+++ Colab-a-list/GEMWEbViewController.h
@@ -0,0 +1,13 @@
+//
+//  GEMWebViewController.h
+//  Colab-a-list
+//
+//  Created by Eric Garcia on 4/9/14.
+//  Copyright (c) 2014 Garcia Enterprise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GEMWebViewController : UIViewController
+
+@end
--- Colab-a-list/GEMWEbViewController.m
+++ Colab-a-list/GEMWEbViewController.m
@@ -1,19 +1,18 @@
 //
-//  GEMAboutViewController.m
+//  GEMWebViewController.m
 //  Colab-a-list
 //
-//  Created by Eric Garcia on 4/7/14.
+//  Created by Eric Garcia on 4/9/14.
 //  Copyright (c) 2014 Garcia Enterprise. All rights reserved.
 //
 
-#import "GEMAboutViewController.h"
-#import "SWRevealViewController.h"
+#import "GEMWebViewController.h"
 
-@interface GEMAboutViewController ()
+@interface GEMWebViewController ()
 
 @end
 
-@implementation GEMAboutViewController
+@implementation GEMWebViewController
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -28,30 +27,12 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    
-    // Set the side bar button action. When it's tapped, it'll show up the sidebar.
-    _sidebarButton.target = self.revealViewController;
-    _sidebarButton.action = @selector(revealToggle:);
-    
-    // Set the gesture
-    [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-#pragma mark - Buttons
-
-- (IBAction)googlePlusLink:(id)sender {
-}
-
-- (IBAction)facebookLink:(id)sender {
-}
-
-- (IBAction)twitterLink:(id)sender {
 }
 
 /*
