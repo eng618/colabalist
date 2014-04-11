7837dc7ee8c34f0126c16cbd8de43f185bf23573
eng618 <eng618@fullsail.edu>
2014-04-10 22:47:59 -0400

    Added code to display shopping list items

--- Colab-a-list/GEMShoppingListTableViewController.m
+++ Colab-a-list/GEMShoppingListTableViewController.m
@@ -36,8 +36,21 @@
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
     
     // Load dummy data into array to test app
-    GEMItem *item = [[GEMItem alloc] init];
+    //GEMItem *item = [[GEMItem alloc] init];
     //item.item = item.text
+    
+    // Return the number of sections.
+    GEMItemManager *manager = [GEMItemManager storedItems];
+    if (manager) {
+        GEMItem *item = [[GEMItem alloc] init];
+        item.item = @"Milk";
+        item.qty = @"1 Gallon";
+        item.category = @"Dairy";
+        item.notes = @"Milk does a body good!";
+        
+        [manager.items addObject:item];
+    }
+    
     
     
     // Set the gesture
@@ -52,7 +65,7 @@
 
 - (void) viewDidAppear:(BOOL)animated
 {
-    
+    [self.tableView reloadData];
 }
 
 - (void)didReceiveMemoryWarning
@@ -66,25 +79,47 @@
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
     // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     // Return the number of rows in the section.
-    return 0;
+    int rows = 0;
+    // Create instance of ItemManager
+    GEMItemManager *manager = [GEMItemManager storedItems];
+    // Check validity
+    if (manager) {
+        // Return number of items in array to populate table view
+        rows = [manager.items count];
+    }
+    return rows;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"shoppingCell" forIndexPath:indexPath];
     
     // Configure the cell...
+    // Create instance of ItemManager
+    GEMItemManager *manager = [GEMItemManager storedItems];
+    // Check validity
+    if (manager) {
+        // Create instence of items array
+        NSMutableArray *items = [manager items];
+        GEMItem *current = [items objectAtIndex:indexPath.row];
+        
+        // Set text
+        cell.textLabel.text = [current item];
+        cell.detailTextLabel.text = [current qty];
+        
+        
+        
+    }
     
     return cell;
 }
-*/
 
 /*
 // Override to support conditional editing of the table view.
